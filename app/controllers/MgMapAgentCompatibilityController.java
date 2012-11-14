67b9783b82fb1b975134c2a6d77ccd508858f457
Jackie Ng <jackie.ng@ennoble.com.au>
2012-11-14 18:44:59 +1100

    Add mapagent compatibility stub

---
+++ app/controllers/MgMapAgentCompatibilityController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import actions.*;
+import play.*;
+import play.mvc.*;
+import java.lang.StringBuilder;
+
+import org.osgeo.mapguide.*;
+
+@MgCheckSession
+public abstract class MgMapAgentCompatibilityController extends MgAbstractAuthenticatedController {
+
+    public static Result processGetRequest() {
+        return TODO;
+    }
+    
+    public static Result processPostRequest() {
+        return TODO;
+    }
+
+}
